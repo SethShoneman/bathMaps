f860a16e497052745ca3215ae2ba24786f915dee
Seth Shoneman <60634819+SethShoneman@users.noreply.github.com>
2021-02-13 12:22:57 -0500

    Update plotlyExpress.py

--- plotlyExpress.py
+++ plotlyExpress.py
@@ -3,6 +3,8 @@ import repository as rep
 
 token = "pk.eyJ1IjoidWdoaXRzc2lkIiwiYSI6ImNrNm54Z3I5cTE1aDIzbW55MjcwdWp4MnEifQ.xylSSPUA0Yt9ly6jOBMg4w"
 
+spaces='                                                                                                           '
+
 lats = [rep.codeDay.lat, rep.sb1.lat, rep.chpt1.lat, rep.pnbrd1.lat]
 lons = [rep.codeDay.lon, rep.sb1.lon, rep.chpt1.lon, rep.pnbrd1.lon]
 texts = [rep.codeDay.text, rep.sb1.text, rep.chpt1.text, rep.pnbrd1.text]
@@ -31,10 +33,68 @@ fig.update_layout(
     ),
 )
 
-def open():
-    print("hi")
+import json
+from textwrap import dedent as d
 
-print(fig.data)
-# point = fig.data[]
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
 
-fig.show()
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div([
+    dcc.Graph(
+        id='basic-interactions',
+        figure=fig
+    ),
+
+    # dcc.Link(
+    #     'Navigate to "Leave a Review"',
+    #     href='https://docs.google.com/forms/d/e/1FAIpQLSdA-ITHtdfyulyhr2ZOlWZ624AErDcd1gvJDaro22RtleY-zw/viewform'
+    # ),
+
+    html.Div(className='row', children=[
+         html.Div([
+             dcc.Markdown(d("")),
+             html.Pre(id='hover-data')
+        ], className='three columns'),
+
+        html.Div([
+            dcc.Markdown(d("")),
+            html.Pre(id='click-data'),
+        ], className='three columns',)
+    ])
+])
+
+
+@app.callback(
+    Output('hover-data', 'children'),
+    [Input('basic-interactions', 'hoverData')])
+def display_hover_data(hoverData):
+    if hoverData == None:
+        print('None')
+    else:
+        x = hoverData['points']
+        x = x[0]
+        out = (spaces+str(x['lon']) + ', ' + str(x['lat']) + ' - ' + x['text'])
+        return json.dumps(out)
+
+@app.callback(
+    Output('click-data', 'children'),
+    [Input('basic-interactions', 'clickData')])
+
+def click_to_page(clickData):
+    if clickData == None:
+        print('None')
+    else:
+        x = clickData['points']
+        x = x[0]
+        print(x['text'])
+        return dcc.Link(
+        "Leave a Review",
+        href='https://docs.google.com/forms/d/e/1FAIpQLSdA-ITHtdfyulyhr2ZOlWZ624AErDcd1gvJDaro22RtleY-zw/viewform')
+
+app.run_server(debug=True)
